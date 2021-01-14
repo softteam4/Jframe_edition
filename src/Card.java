c3d2ae897a622565b12999ade2e693072802798b
Hoshi Iju <e195713@ie.u-ryukyu.ac.jp>
2021-01-14 11:44:35 +0900

    日付機能をクラス化（Module_deadline） by 195713K

--- src/Card.java
+++ src/Card.java
@@ -6,19 +6,11 @@ import java.awt.event.ActionListener;
 public class Card extends JPanel {
     Card(){
         this.setLayout(new BorderLayout());
-        deadline = new JPanel();
+        deadline = new Module_deadline();
         main_panel = new JPanel();
 
         title = new JTextField(20);
         removeButton = new JButton("削除");
-
-        month = new JComboBox(month_data);
-        day = new JComboBox(day_data);
-
-        deadline.add(month);
-        deadline.add(new JLabel("月"));
-        deadline.add(day);
-        deadline.add(new JLabel("日"));
 
         removeButton.addActionListener(new ActionListener() {
             @Override
@@ -36,12 +28,8 @@ public class Card extends JPanel {
     JTextField title;
     JButton removeButton;
 
-    JComboBox month;
-    JComboBox day;
     JPanel main_panel;
-    JPanel deadline;
-    String[] month_data = {"1","2","3","4","5","6","7","8","9","10","11","12"};
-    String[] day_data ={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+    Module_deadline deadline;
 
     JScrollPane sp = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     //JTextArea memo;
---
+++ src/Module_deadline.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Module_deadline extends JPanel {
+    Module_deadline(){
+        month = new JComboBox(month_data);
+        day = new JComboBox(day_data);
+
+        this.add(month);
+        this.add(new JLabel("月"));
+        this.add(day);
+        this.add(new JLabel("日"));
+    }
+
+
+    JComboBox month;
+    JComboBox day;
+    String[] month_data = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+    String[] day_data ={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+}
